fc4831ede1ab5f1370d47bbe2e43fc28b6ac698c
daehwa <daehwa001210@gmail.com>
2024-03-27 12:45:42 +0900

    add initialize func

--- model/agent.py
+++ model/agent.py
@@ -3,10 +3,26 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
+class AirfoilCNN(nn.Module):
+    def __init__(self):
+        super(AirfoilCNN, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=2, out_channels=64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(
+            in_channels=64, out_channels=128, kernel_size=3, padding=1
+        )
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(128 * 50, 128)  # 입력 길이가 200으로 가정
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 첫 번째 컨볼루션 및 풀링
+        x = self.pool(F.relu(self.conv2(x)))  # 두 번째 컨볼루션 및 풀링
+        x = x.view(-1, 128 * 50)  # Flatten
+        x = F.relu(self.fc1(x))
+        return x
+
+
 class PPONetwork(nn.Module):
-    def __init__(
-        self, input_dim, ninp, nhead, nhid, nlayers, dropout=0.5
-    ):  # 필요한 파라미터들
+    def __init__(self):
         super(PPONetwork, self).__init__()
         self.encoder = AirfoilCNN()
         # policy network
@@ -15,6 +31,7 @@ class PPONetwork(nn.Module):
         )
         # value network
         self.value = nn.Sequential(nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 1))
+        self._initialize_weights()
 
         # Optimizer
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
@@ -25,22 +42,10 @@ class PPONetwork(nn.Module):
         value = self.value(x)
         return policy, value
 
-
-class AirfoilCNN(nn.Module):
-    def __init__(self):
-        super(AirfoilCNN, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=2, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(
-            in_channels=64, out_channels=128, kernel_size=3, padding=1
-        )
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(
-            128 * 50, 128
-        )  # 차원 조정이 필요함. 200 -> 100 (MaxPooling) -> 50 (두 번째 MaxPooling)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))  # 첫 번째 컨볼루션 및 풀링
-        x = self.pool(F.relu(self.conv2(x)))  # 두 번째 컨볼루션 및 풀링
-        x = x.view(-1, 128 * 50)  # Flatten
-        x = F.relu(self.fc1(x))
-        return x
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
