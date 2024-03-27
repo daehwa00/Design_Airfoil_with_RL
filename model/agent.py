c92f798bc329a5d5b46be3d00ebf72334f3b8aa1
daehwa <daehwa001210@gmail.com>
2024-03-27 12:36:57 +0900

    apply 1D conv

--- model/agent.py
+++ model/agent.py
@@ -8,15 +8,13 @@ class PPONetwork(nn.Module):
         self, input_dim, ninp, nhead, nhid, nlayers, dropout=0.5
     ):  # 필요한 파라미터들
         super(PPONetwork, self).__init__()
-        self.encoder = AirfoilCNN(
-            input_dim, ninp, nhead, nhid, nlayers, dropout
-        )  # 앞서 정의한 TransformerModel 사용
+        self.encoder = AirfoilCNN()
         # policy network
         self.policy = nn.Sequential(
-            nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 3), nn.Softmax(dim=-1)
+            nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 2), nn.Softmax(dim=-1)
         )
         # value network
-        self.value = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 1))
+        self.value = nn.Sequential(nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 1))
 
         # Optimizer
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
@@ -37,14 +35,12 @@ class AirfoilCNN(nn.Module):
         )
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(
-            128 * 50, 100
+            128 * 50, 128
         )  # 차원 조정이 필요함. 200 -> 100 (MaxPooling) -> 50 (두 번째 MaxPooling)
-        self.fc2 = nn.Linear(100, 1)  # 최종 출력, 예를 들어 양력 계수
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))  # 첫 번째 컨볼루션 및 풀링
         x = self.pool(F.relu(self.conv2(x)))  # 두 번째 컨볼루션 및 풀링
         x = x.view(-1, 128 * 50)  # Flatten
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
         return x
