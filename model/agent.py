85b8d8e85ec607ed0e67f6eceaaed6200627ffdb
daehwa <daehwa001210@gmail.com>
2024-03-27 12:35:03 +0900

    add 1D CNN

--- model/agent.py
+++ model/agent.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 
 class PPONetwork(nn.Module):
@@ -7,7 +8,7 @@ class PPONetwork(nn.Module):
         self, input_dim, ninp, nhead, nhid, nlayers, dropout=0.5
     ):  # 필요한 파라미터들
         super(PPONetwork, self).__init__()
-        self.encoder = TransformerEncoderModel(
+        self.encoder = AirfoilCNN(
             input_dim, ninp, nhead, nhid, nlayers, dropout
         )  # 앞서 정의한 TransformerModel 사용
         # policy network
@@ -25,3 +26,25 @@ class PPONetwork(nn.Module):
         policy = self.policy(x)
         value = self.value(x)
         return policy, value
+
+
+class AirfoilCNN(nn.Module):
+    def __init__(self):
+        super(AirfoilCNN, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=2, out_channels=64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(
+            in_channels=64, out_channels=128, kernel_size=3, padding=1
+        )
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(
+            128 * 50, 100
+        )  # 차원 조정이 필요함. 200 -> 100 (MaxPooling) -> 50 (두 번째 MaxPooling)
+        self.fc2 = nn.Linear(100, 1)  # 최종 출력, 예를 들어 양력 계수
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 첫 번째 컨볼루션 및 풀링
+        x = self.pool(F.relu(self.conv2(x)))  # 두 번째 컨볼루션 및 풀링
+        x = x.view(-1, 128 * 50)  # Flatten
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
