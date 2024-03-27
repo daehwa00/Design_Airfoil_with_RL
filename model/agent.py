9ea2337a59053c6019f5b4e1402e96a1326bd757
daehwa <daehwa001210@gmail.com>
2024-03-27 12:33:26 +0900

    remvoe transformer architecture

--- model/agent.py
+++ model/agent.py
@@ -1,27 +1,22 @@
-from .transformer import TransformerEncoderModel
-
 import torch.nn as nn
 import torch.optim as optim
 
 
 class PPONetwork(nn.Module):
-    def __init__(self, input_dim, ninp, nhead, nhid, nlayers, dropout=0.5):  # 필요한 파라미터들
+    def __init__(
+        self, input_dim, ninp, nhead, nhid, nlayers, dropout=0.5
+    ):  # 필요한 파라미터들
         super(PPONetwork, self).__init__()
-        self.encoder = TransformerEncoderModel(input_dim, ninp, nhead, nhid, nlayers, dropout)  # 앞서 정의한 TransformerModel 사용
+        self.encoder = TransformerEncoderModel(
+            input_dim, ninp, nhead, nhid, nlayers, dropout
+        )  # 앞서 정의한 TransformerModel 사용
         # policy network
         self.policy = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 3),
-            nn.Softmax(dim=-1)
+            nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 3), nn.Softmax(dim=-1)
         )
         # value network
-        self.value = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 1)
-        )
-        
+        self.value = nn.Sequential(nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 1))
+
         # Optimizer
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
 
@@ -30,4 +25,3 @@ class PPONetwork(nn.Module):
         policy = self.policy(x)
         value = self.value(x)
         return policy, value
-    
