67590e671b4b036ede55d59c878e57d8f9985344
daehwa <daehwa001210@gmail.com>
2024-06-03 21:04:32 +0900

    add model layers

--- model/model.py
+++ model/model.py
@@ -6,22 +6,13 @@ import torch.nn.functional as F
 class AirfoilCNN(nn.Module):
     def __init__(self):
         super(AirfoilCNN, self).__init__()
-        self.conv1 = nn.Conv2d(
-            1, 16, kernel_size=7, stride=2, padding=3
-        )  # 340x240 -> 170x120
-        self.conv2 = nn.Conv2d(
-            16, 32, kernel_size=7, stride=2, padding=3
-        )  # 170x120 -> 85x60
-        self.conv3 = nn.Conv2d(
-            32, 64, kernel_size=5, stride=2, padding=2
-        )  # 85x60 -> 43x30
-        self.conv4 = nn.Conv2d(
-            64, 128, kernel_size=5, stride=2, padding=2
-        )  # 43x30 -> 22x15
-        self.fc1 = nn.Linear(
-            128 * 22 * 15, 128
-        )  # 최종 컨볼루션 레이어의 출력 크기를 바탕으로 계산된 값
-        self.fc2 = nn.Linear(128, 64)  # 추가적인 FC 레이어
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(4, 8), stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 7), stride=2)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(4, 7), stride=2)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(4, 8), stride=2)
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 4), stride=2)
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(2, 3), stride=1)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
 
     def forward(self, x):
         if len(x.shape) == 3:
@@ -30,9 +21,10 @@ class AirfoilCNN(nn.Module):
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1) 
         return x
 
 
@@ -41,7 +33,7 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.encoder = AirfoilCNN()  # 상태를 인코딩하는 네트워크
         self.policy_mean = nn.Sequential(
-            nn.Linear(64, 64), nn.ReLU(), nn.Linear(64, n_action)
+            nn.Linear(512, 64), nn.ReLU(), nn.Linear(64, n_action)
         )
         self.policy_std = nn.Parameter(torch.zeros(n_action))
         self._initialize_weights()
@@ -67,7 +59,7 @@ class Critic(nn.Module):
     def __init__(self):
         super(Critic, self).__init__()
         self.encoder = AirfoilCNN()  # 상태를 인코딩하는 네트워크
-        self.value = nn.Sequential(nn.Linear(64, 64), nn.ReLU(), nn.Linear(64, 1))
+        self.value = nn.Sequential(nn.Linear(512, 64), nn.ReLU(), nn.Linear(64, 1))
         self._initialize_weights()
 
     def forward(self, state):
